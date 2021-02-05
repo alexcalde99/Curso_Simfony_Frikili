06fbf8c089e96791540620e5422bb9e8d235c919
Alex Calderón <alexcalde999@gmail.com>
2021-02-05 12:55:35 +0100

    13. Curso de Symfony 5 - Métodos de consulta a la Base de Datos

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Posts;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class DashboardController extends AbstractController
      */
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Posts::class)->findAll();
+        $post = $em->getRepository(Posts::class)->findAll();
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'Bienvenido a DashboardController',
+            'posts'=>$posts
 
         ]);
     }
